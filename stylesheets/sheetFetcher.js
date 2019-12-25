40bd39c16aba3e09523ff77baab7d665fa193a2b
Asa Dillahunty <stantondillahunty@gmail.com>
2019-12-25 03:32:20 -0600

    removed one I added

--- stylesheets/sheetFetcher.js
+++
@@ -1,8 +0,0 @@
-function init() {
-    Tabletop.init( { key: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRZ5LtUDxChRQV-1DFQyUxb4U74kJ0ZAANlexB9IW7sFnkKTzwS8Lq9P1A3Ryvma7Mfvi6zd9gf0Ioq/pubhtml?widget=true&amp;headers=false",
-    callback: function(data, tabletop) {
-        console.log(data)
-    },
-    simpleSheet: true } )
-}
-window.addEventListener('DOMContentLoaded', init)
