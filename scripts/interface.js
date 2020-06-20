13d705fba85a1828cb038f2fc11902e90666b829
Asa Dillahunty <stantondillahunty@gmail.com>
2020-06-19 23:53:23 -0500

    Game heavily improved
    Having a lot of difficulty.The AI could be messing up the board. Need to create a simple one that doesn't even look into future moves

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,3 +1,10 @@
+var game_status = 'not started';
+var gameBoard;
+var turn;
+var turnCount;
+var players;
+
+
 var input = document.getElementById("message");
 input.addEventListener("keyup", function(event) {
 	// Number 13 is the "Enter" key on the keyboard
@@ -5,13 +12,15 @@ input.addEventListener("keyup", function(event) {
 	  // Trigger the button element with a click
 	  document.getElementById("sendMessage").click();
 	}
-  });
+});
 
 function sendMessage() {
 	var msg = document.getElementById('message').value;
-	document.getElementById("perm").innerHTML += "<div class=letter type=home><p class='home'>" + msg + "</p><div>";
 	document.getElementById('message').value = '';
-	//need to find how to scroll to bottom of #perm when clicked
+	
+	if (game_status == 'not started') {
+		return; // IDK
+	}
 
 	//parse the entered numbers
 	var list = msg.split(",");
@@ -19,14 +28,207 @@ function sendMessage() {
 	var col = parseInt(list[1]) - 1;
 	var row = parseInt(list[2]) - 1;
 
-	markCell(slice,row,col,"X");
+	if (turnCount > 63) {
+		alert('Tie Game!');
+	}
+	if (validMove(slice,col,row) == false) {
+		return; // IDK
+	}
 
+
+	markCell(slice,row,col,players[turn]);
+	gameBoard[slice][row][col] = players[turn];
+
+
+	if (getWinner() != 0) {
+		console.log(players[turn] + ' wins!')
+		game_status = 'finshed';
+		alert(players[turn] + ' wins!')
+	}
+	turn = (turn+1)%2;
+	turnCount++;
+	
+	// getMove();
+
+	// if (getWinner() != 0) {
+	// 	console.log(players[turn] + ' wins!')
+	// 	game_status = 'finshed';
+	// 	alert(players[turn] + ' wins!')
+	// }
+	// turn = (turn+1)%2;
+	// turnCount++;
+}
+
+function boardInit() {
+	// Clears the html board
+	for (var i=0;i<4;i++)
+		for (var j=0;j<4;j++)
+			for (var k=0;k<4;k++) {
+				var cell = document.getElementsByClassName("slice")[i].children[4].children[j].children[k];
+				cell.innerHTML='&nbsp;';
+				cell.classList='';
+			}
+
+	//creates an empty board array
+	var board = [];
+	for (var i=0;i<4;i++) {
+		var slice = [];
+		for (var j=0;j<4;j++) {
+			var row = [' ',' ',' ',' '];
+			slice.push(row);
+		}
+		board.push(slice);
+	}
+	return board;
+}
+
+function validMove(i,j,k) {
+	if (i>3 || j>3 || k>3) return false;
+	if (i<0 || j<0 || k<0) return false;
+
+	if (gameBoard[i][j][k] == ' ') return true;
+	else return false;
 }
 
 function markCell(i,j,k,mark) {
+	console.log("marking "+i+' '+j+' '+k);
 	var cell = document.getElementsByClassName("slice")[i].children[4].children[j].children[k];
 
 	cell.innerHTML = mark;
 	cell.classList += mark;
+}
+
+function getWinner() {
+	for (var i=0;i<4;i++)
+		for (var j=0;j<4;j++)
+			for (var k=0;k<4;k++)
+				for(var dx=-1;dx<=1;dx++)
+					for(var dy=-1;dy<=1;dy++)
+						for(var dz=-1;dz<=1;dz++)
+						{
+							if (dx==0 && dy==0 && dz==0) continue;
+							if (i+dx*3 > 3 || i+dx*3 < 0) continue;
+							if (j+dy*3 > 3 || j+dy*3 < 0) continue;
+							if (k+dz*3 > 3 || k+dz*3 < 0) continue;
+								if(gameBoard[i][j][k]!=' ' && 
+									gameBoard[i][j][k]==gameBoard[i+dx][j+dy][k+dz] && 
+									gameBoard[i][j][k]==gameBoard[i+2*dx][j+2*dy][k+2*dz] && 
+									gameBoard[i][j][k]==gameBoard[i+3*dx][j+3*dy][k+3*dz]) {
+
+										document.getElementsByClassName("slice")[i].children[4].children[j].children[k].classList+=' winning-move';
+										document.getElementsByClassName("slice")[i+dx].children[4].children[j+dy].children[k+dz].classList+=' winning-move';
+										document.getElementsByClassName("slice")[i+dx*2].children[4].children[j+dy*2].children[k+dz*2].classList+=' winning-move';
+										document.getElementsByClassName("slice")[i+dx*3].children[4].children[j+dy*3].children[k+dz*3].classList+=' winning-move';
+
+										return gameBoard[i][j][k];
+									}
+
+						}
+	return 0;
+}
+
+function startGame() {
+	game_status = 'in progress';
+	gameBoard = boardInit();
+	players = ['X','O'];
+	turn = 0;
+}
+
+function getMoveR() {
 
 }
+
+
+// Bot Code //
+
+function getMove() {
+	var result = canWin();
+	if (result != 0) markCell(result[0],result[1],result[2]);
+
+	console.log("making it to score")
+
+	var highScore = -1;
+	var bestMove = [0,0,0];
+
+	for(var i=0;i<4;i++)
+		for(var j=0;j<4;j++)
+			for(var k=0;k<4;k++) {
+				if (!validMove(i,j,k)) continue;
+				
+				score = getScore(i,j,k);
+				if (score < highScore) continue;
+				if (score == highScore) 
+					if (Math.random() < .5) continue;
+
+				bestMove[0] = i;
+				bestMove[1] = j;
+				bestMove[2] = k;
+			}
+
+	markCell(bestMove[0],bestMove[1],bestMove[2],players[turn]);
+	gameBoard[bestMove[0]][bestMove[1]][bestMove[2]] = players[turn];
+}
+
+function canWin() {
+	for (var i=0;i<4;i++)
+		for (var j=0;j<4;j++)
+			for (var k=0;k<4;k++) {
+				if (validMove(i,j,k)) gameBoard[i][j][k] = players[turn];
+				else continue;
+
+				if (getWinner() != 0) {
+					gameBoard[i][j][k] = ' ';
+					return [i,j,k];
+				}
+				else gameBoard[i][j][k] = ' ';
+			}
+	return 0;
+}
+
+function getScore(x,y,z) {
+	var OTHER_POINTS = 10;
+	var THIS_POINTS = 10;
+
+	var OTHER;
+	var THIS;
+
+	var rowScore = 1;
+	var score = 0;
+
+	for(var dx=-1;dx<=1;dx++)
+		for(var dy=-1;dy<=1;dy++)
+			for(var dz=-1;dz<=1;dz++) {
+				if (dx==0 && dy==0 && dz==0) continue;
+
+				rowScore = 1;
+				OTHER = false;
+				THIS = false;
+
+				for (var i=-4;i<4;i++) {
+					if (i==0) continue; // We know this space is blank
+					
+					if (x+dx*i > 3 || x+dx*i < 0) continue;
+					if (y+dy*i > 3 || y+dy*i < 0) continue;
+					if (z+dz*i > 3 || z+dz*i < 0) continue;
+					
+					if (gameBoard[x + i*dx][y + i*dy][z + i*dz] == players[turn]) {
+						rowScore *= THIS_POINTS;
+						// System.out.println("Me: "+(x+i*dx) +","+(y+i*dx) +","+(z+i*dx));
+						THIS = true;
+					}
+					else if (gameBoard[x + i*dx][y + i*dy][z + i*dz] == players[(turn+1)%2]) {
+						rowScore *= OTHER_POINTS;
+						// System.out.println("Them: "+(x+i*dx) +","+(y+i*dx) +","+(z+i*dx));
+						OTHER = true;
+					}
+				}
+				if (THIS && OTHER) continue; // if it has both, neither of us can win that row
+				score += rowScore;
+			}
+			
+	if (x==0||y==0||z==0||x==3||y==3||z==3) score++;
+	console.log(score);
+	return score;
+}
+
+
